232c902fbbd91fc2ed5e7d53921263a018607793
Birkhoff <birkhoffcheng@gmail.com>
2018-08-02 23:05:23 -0400

    Changed type

--- lightsout/lightsout.c
+++ lightsout/lightsout.c
@@ -9,7 +9,7 @@
 #include "lib/xalloc.h"
 #include "board-ht.h"
 
-uint32_t power(uint32_t base, uint32_t exp) {
+unsigned int power(unsigned int base, unsigned int exp) {
 	if (exp == 0)
 		return 1;
 	else {
@@ -21,7 +21,7 @@ int bruteforce(bitvector bv, uint8_t width, uint8_t height) {
     bitvector solution, bv_orig = bv;
     uint8_t bit_index;
     uint8_t row, col;
-    for (solution = 0; solution < power(2, width * height); solution++) {
+    for (solution = bitvector_new(); solution < power(2, width * height); solution++) {
         bv = bv_orig;
         for (bit_index = 0; bit_index < width * height; bit_index++) {
             if (bitvector_get(solution, bit_index)) {
@@ -38,7 +38,7 @@ int bruteforce(bitvector bv, uint8_t width, uint8_t height) {
                     bv = bitvector_flip(bv, bit_index + 1);
             }
         }
-        if (bv == 0) {
+        if (bitvector_equal(bv, bitvector_new())) {
 //			printf("\nBoard is solvable.\nSolution bitmap (the squares marked # need to be pushed to solve):\n");
 //			print_board(solution, width, height);
 
