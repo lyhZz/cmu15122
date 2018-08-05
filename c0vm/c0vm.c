0a711705a795bce0935be3855c524f771a6bdda8
Birkhoff <birkhoffcheng@gmail.com>
2018-08-05 14:32:32 -0400

    Task 1 v0.1

--- c0vm/c0vm.c
+++ c0vm/c0vm.c
@@ -14,176 +14,280 @@
 /* call stack frames */
 typedef struct frame_info frame;
 struct frame_info {
-  c0v_stack_t S; /* Operand stack of C0 values */
-  ubyte *P;      /* Function body */
-  size_t pc;     /* Program counter */
-  c0_value *V;   /* The local variables */
+	c0v_stack_t S;	/* Operand stack of C0 values */
+	ubyte *P;		/* Function body */
+	size_t pc;		/* Program counter */
+	c0_value *V;	/* The local variables */
 };
 
+int pop_int(c0v_stack_t S) {
+	return val2int(c0v_pop(S));
+}
+
+void push_int(c0v_stack_t S, int i) {
+	c0v_push(S, int2val(i));
+}
+
+void *pop_ptr(c0v_stack_t S) {
+	return val2ptr(c0v_pop(S));
+}
+
+void push_ptr(c0v_stack_t S, void *ptr) {
+	c0v_push(S, ptr2val(ptr));
+}
+
 int execute(struct bc0_file *bc0) {
-  REQUIRES(bc0 != NULL);
+	REQUIRES(bc0 != NULL);
 
-  /* Variables */
-  c0v_stack_t S; /* Operand stack of C0 values */
-  ubyte *P;      /* Array of bytes that make up the current function */
-  size_t pc;     /* Current location within the current byte array P */
-  c0_value *V;   /* Local variables (you won't need this till Task 2) */
-  (void) V;
+	/* Variables */
+	c0v_stack_t S;	/* Operand stack of C0 values */
+	ubyte *P;		/* Array of bytes that make up the current function */
+	size_t pc;		/* Current location within the current byte array P */
+	c0_value *V;	/* Local variables (you won't need this till Task 2) */
+	(void) V;
 
-  /* The call stack, a generic stack that should contain pointers to frames */
-  /* You won't need this until you implement functions. */
-  gstack_t callStack;
-  (void) callStack;
+	/* The call stack, a generic stack that should contain pointers to frames */
+	/* You won't need this until you implement functions. */
+	gstack_t callStack;
+	(void) callStack;
 
-  while (true) {
+	while (true) {
 
 #ifdef DEBUG
-    /* You can add extra debugging information here */
-    fprintf(stderr, "Opcode %x -- Stack size: %zu -- PC: %zu\n",
-            P[pc], c0v_stack_size(S), pc);
+		/* You can add extra debugging information here */
+		fprintf(stderr, "Opcode %x -- Stack size: %zu -- PC: %zu\n", P[pc], c0v_stack_size(S), pc);
 #endif
 
-    switch (P[pc]) {
+		switch (P[pc]) {
 
-    /* Additional stack operation: */
+		/* Additional stack operation: */
 
-    case POP: {
-      pc++;
-      c0v_pop(S);
-      break;
-    }
+		case POP: {
+			pc++;
+			c0v_pop(S);
+			break;
+		}
 
-    case DUP: {
-      pc++;
-      c0_value v = c0v_pop(S);
-      c0v_push(S,v);
-      c0v_push(S,v);
-      break;
-    }
+		case DUP: {
+			pc++;
+			c0_value v = c0v_pop(S);
+			c0v_push(S,v);
+			c0v_push(S,v);
+			break;
+		}
 
-    case SWAP:
+		case SWAP: {
+			pc++;
+			c0_value x = c0v_pop(S);
+			c0_value y = c0v_pop(S);
+			c0v_push(S, x);
+			c0v_push(S, y);
+			break;
+		}
 
 
-    /* Returning from a function.
-     * This currently has a memory leak! You will need to make a slight
-     * change for the initial tasks to avoid leaking memory.  You will
-     * need to be revise it further when you write INVOKESTATIC. */
+		/* Returning from a function.
+		 * This currently has a memory leak! You will need to make a slight
+		 * change for the initial tasks to avoid leaking memory.	You will
+		 * need to be revise it further when you write INVOKESTATIC. */
 
-    case RETURN: {
-      int retval = val2int(c0v_pop(S));
-      assert(c0v_stack_empty(S));
+		case RETURN: {
+			int retval = val2int(c0v_pop(S));
+			assert(c0v_stack_empty(S));
 #ifdef DEBUG
-      fprintf(stderr, "Returning %d from execute()\n", retval);
+			fprintf(stderr, "Returning %d from execute()\n", retval);
 #endif
-      // Free everything before returning from the execute function!
-      return retval;
-    }
+			c0v_stack_free(S);
+			return retval;
+		}
 
 
-    /* Arithmetic and Logical operations */
+		/* Arithmetic and Logical operations */
 
-    case IADD:
+		case IADD: {
+			pc++;
+			uint32_t a = pop_int(S);
+			uint32_t b = pop_int(S);
+			uint32_t result = a + b;
+			push_int(S, (int32_t)result);
+			break;
+		}
 
-    case ISUB:
+		case ISUB: {
+			pc++;
+			uint32_t b = pop_int(S);
+			uint32_t a = pop_int(S);
+			uint32_t result = a - b;
+			push_int(S, (int32_t)result);
+			break;
+		}
 
-    case IMUL:
+		case IMUL: {
+			pc++;
+			uint32_t a = pop_int(S);
+			uint32_t b = pop_int(S);
+			uint32_t result = a * b;
+			push_int(S, (int32_t)result);
+			break;
+		}
 
-    case IDIV:
+		case IDIV: {
+			pc++;
+			int b = pop_int(S);
+			int a = pop_int(S);
+			int result = a / b;
+			push_int(S, result);
+			break;
+		}
 
-    case IREM:
+		case IREM: {
+			pc++;
+			int b = pop_int(S);
+			int a = pop_int(S);
+			int result = a % b;
+			push_int(S, result);
+			break;
+		}
 
-    case IAND:
+		case IAND: {
+			pc++;
+			int a = pop_int(S);
+			int b = pop_int(S);
+			int result = a & b;
+			push_int(S, result);
+			break;
+		}
 
-    case IOR:
+		case IOR: {
+			pc++;
+			int a = pop_int(S);
+			int b = pop_int(S);
+			int result = a | b;
+			push_int(S, result);
+			break;
+		}
 
-    case IXOR:
+		case IXOR: {
+			pc++;
+			int a = pop_int(S);
+			int b = pop_int(S);
+			int result = a ^ b;
+			push_int(S, result);
+			break;
+		}
 
-    case ISHL:
+		case ISHL: {
+			pc++;
+			int b = pop_int(S);
+			int a = pop_int(S);
+			int result = a << b;
+			push_int(S, result);
+			break;
+		}
 
-    case ISHR:
-
-
-    /* Pushing constants */
-
-    case BIPUSH:
-
-    case ILDC:
-
-    case ALDC:
-
-    case ACONST_NULL:
-
-
-    /* Operations on local variables */
-
-    case VLOAD:
-
-    case VSTORE:
-
-
-    /* Control flow operations */
-
-    case NOP:
-
-    case IF_CMPEQ:
-
-    case IF_CMPNE:
-
-    case IF_ICMPLT:
-
-    case IF_ICMPGE:
-
-    case IF_ICMPGT:
-
-    case IF_ICMPLE:
-
-    case GOTO:
-
-    case ATHROW:
-
-    case ASSERT:
-
-
-    /* Function call operations: */
-
-    case INVOKESTATIC:
-
-    case INVOKENATIVE:
-
-
-    /* Memory allocation operations: */
-
-    case NEW:
-
-    case NEWARRAY:
-
-    case ARRAYLENGTH:
+		case ISHR: {
+			pc++;
+			int b = pop_int(S);
+			int a = pop_int(S);
+			int result = a >> b;
+			push_int(S, result);
+			break;
+		}
 
 
-    /* Memory access operations: */
+		/* Pushing constants */
 
-    case AADDF:
+		case BIPUSH: {
+			push_int(S, P[pc+1]);
+			pc += 2;
+			break;
+		}
 
-    case AADDS:
+		case ILDC: {
+			push_int(S, int_pool[(P[pc+1]<<8)|P[pc+2]]);
+			pc += 3;
+			break;
+		}
 
-    case IMLOAD:
+		case ALDC: {
+			push_ptr(S, &string_pool[(P[pc+1]<<8)|P[pc+2]]);
+			pc += 3;
+			break;
+		}
 
-    case IMSTORE:
+		case ACONST_NULL:
 
-    case AMLOAD:
 
-    case AMSTORE:
+		/* Operations on local variables */
 
-    case CMLOAD:
+		case VLOAD:
 
-    case CMSTORE:
+		case VSTORE:
 
-    default:
-      fprintf(stderr, "invalid opcode: 0x%02x\n", P[pc]);
-      abort();
-    }
-  }
 
-  /* cannot get here from infinite loop */
-  assert(false);
+		/* Control flow operations */
+
+		case NOP:
+
+		case IF_CMPEQ:
+
+		case IF_CMPNE:
+
+		case IF_ICMPLT:
+
+		case IF_ICMPGE:
+
+		case IF_ICMPGT:
+
+		case IF_ICMPLE:
+
+		case GOTO:
+
+		case ATHROW:
+
+		case ASSERT:
+
+
+		/* Function call operations: */
+
+		case INVOKESTATIC:
+
+		case INVOKENATIVE:
+
+
+		/* Memory allocation operations: */
+
+		case NEW:
+
+		case NEWARRAY:
+
+		case ARRAYLENGTH:
+
+
+		/* Memory access operations: */
+
+		case AADDF:
+
+		case AADDS:
+
+		case IMLOAD:
+
+		case IMSTORE:
+
+		case AMLOAD:
+
+		case AMSTORE:
+
+		case CMLOAD:
+
+		case CMSTORE:
+
+		default:
+			fprintf(stderr, "invalid opcode: 0x%02x\n", P[pc]);
+			abort();
+		}
+	}
+
+	/* cannot get here from infinite loop */
+	assert(false);
 }
